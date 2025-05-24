bce91f856cf32039f099ca5416a59fcee5a8b4ab
Xavier <xdupondpoubelle@example.com>
2025-05-24 19:49:03 +0200

    v13

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@
 //const port = process.env.PORT;
 //const url_mongodb = process.env.DB_URL;
 //const connection_bdd_mongodb_login = process.env.DATABASE_URL;
-const port = process.env.PORT;
+const port = 80;
 const url_mongodb = "mongodb+srv://xthieuleux:Mongodb%4033.com@cluster0.5eej2b2.mongodb.net/?retryWrites=true&w=majority";
 const connection_bdd_mongodb_login = "bdd_le_combat_des_heros_dev";
 const option_captcha={
